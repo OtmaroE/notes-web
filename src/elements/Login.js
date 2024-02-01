46795a3800e2fad19b73f856a6daa6dc033a3315
Jose Gonzalez <otmaro@outlook.com>
2024-01-31 20:43:32 -0600

    Add endpoint call on login

--- src/elements/Login.js
+++ src/elements/Login.js
@@ -2,20 +2,25 @@ import { useState } from 'react';
 
 import './Login.css';
 import { isLoggedIn } from '../security/Protected';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const [loginError, setLoginError] = useState(0);
+  const [loginError, setLoginError] = useState(false); 
 
-  const handleSubmit = (event) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const { username, password } = document.forms[0];
-    if (password.value === 'test' && username.value === 'test') {
-      setLoginError(false);
-      localStorage.setItem('token', 'ey');
-      return <Navigate to='/' />
-    } else {
+    const loginResponse = await fetch(`http://localhost:3030/users/login?email=${username.value}&password=${password.value}`, {
+      method: 'POST',
+    });
+    if(loginResponse.status !== 200) {
       setLoginError(true);
+    } else {
+      setLoginError(false);
+      localStorage.setItem('token', loginResponse.text());
+      navigate('/');
     }
   }
   const loginForm = (
